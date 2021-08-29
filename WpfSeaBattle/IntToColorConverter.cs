85d7876437d386b7a05a4bd395979a2dae648869
dante72 <turbosuns7@gmail.com>
2021-08-29 17:13:46 +0300

    create game frame (MVVM)

---
+++ WpfSeaBattle/IntToColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfSeaBattle
+{
+    public class IntToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch((int)value)
+            {
+                case 0: return Brushes.LightBlue;
+                case 1: return Brushes.White;
+            }
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfSeaBattle/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSeaBattle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var field1 = new Field(10, 10);
+            CreateFieldView(battleField1, field1);
+            CreateFieldView(battleField2, field1, true);
+        }
+
+        static private void CreateFieldView(WrapPanel wp, Field field, bool FogOfWar = false)
+        {
+            for (int i = 0; i < field.VerticalItemsCount; i++)
+                for (int j = 0; j < field.HorizontalItemsCount; j++)
+                {
+                    wp.Children.Add(new ToggleButton()
+                    {
+                        Width = wp.Height / field.VerticalItemsCount,
+                        Height = wp.Width / field.HorizontalItemsCount,
+                        IsChecked = FogOfWar
+                    });
+                }
+
+            for (int i = 0; i < field.VerticalItemsCount; i++)
+                for (int j = 0; j < field.HorizontalItemsCount; j++)
+                {
+                    (wp.Children[i * field.VerticalItemsCount + j] as ToggleButton)
+                        .DataContext = field[i, j];
+                }
+        }
+    }
+}
