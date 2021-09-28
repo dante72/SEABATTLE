73cd70abb2803af62c21775fb64c28f5dbb126a9
4rt1_j0k3r <artijoker@hotmail.com>
2021-09-28 10:55:02 +0300

    message architecture

---
+++ SeaBattleLib/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattleLib {
+
+    public enum GameStatus { DidNotStart, GameIsOn, GameOver, PlayerHasLeftGame }
+    public static class Message {
+        public const byte Сonnection = 1;
+        public const byte GameStatus = 2;
+        public const byte WhoseShot = 3;
+        public const byte Shot = 4;
+        public const byte ChatNotice = 5;
+    }
+}
--- SeaBattleLib/Ship.cs
+++ SeaBattleLib/Ship.cs
@@ -5,34 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 using SeaBattleLib;
 
-namespace SeaBattleLib
-{
-    enum Orientation
-    {
-        Horizontal,
-        Vertical
-    }
-    class Ship
-    {
-        readonly public List<Point> Location = new List<Point>();
-        readonly public List<Point> Area = new List<Point>();
+namespace SeaBattleLib {
+    public enum Orientation { Horizontal, Vertical }
 
-        public Ship(Point head, int partCount = 1, Orientation orientation = Orientation.Horizontal)
-        {
+    public class Ship {
+        public List<Cell> Location { get; } = new List<Cell>();
+        public List<Cell> Area { get; } = new List<Cell>();
+        public bool IsDestroyed { get; set; } = false;
+
+
+        public Ship(Cell head, int partCount = 1, Orientation orientation = Orientation.Horizontal) {
             for (int i = 0; i < partCount; i++)
-                Location.Add(new Point(
+                Location.Add(new Cell(
                                         head.X + (orientation == Orientation.Vertical ? i : 0),
                                         head.Y + (orientation == Orientation.Horizontal ? i : 0),
-                                        (int)Textures.Ship
+                                        Textures.Ship
                                        )
                             );
             for (int i = head.X - 1; i < head.X + (orientation == Orientation.Vertical ? partCount : 1) + 1; i++)
-                for (int j = head.Y - 1; j < head.Y + (orientation == Orientation.Horizontal ? partCount : 1) + 1; j++)
-                    {
-                        var point = new Point(i, j, (int)Textures.Miss);
-                        if (!Location.Any(x => x == point))
-                            Area.Add(point);
-                    }
+                for (int j = head.Y - 1; j < head.Y + (orientation == Orientation.Horizontal ? partCount : 1) + 1; j++) {
+                    var point = new Cell(i, j, Textures.Miss);
+                    if (!Location.Any(x => x == point))
+                        Area.Add(point);
+                }
         }
     }
 }
