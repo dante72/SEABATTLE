5c75760755164e7842e6da2ec5fa0def77d9faf4
4rt1_j0k3r <artijoker@hotmail.com>
2021-09-28 10:55:02 +0300

    message architecture

---
+++ SeaBattleLib/Cell.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattleLib {
+    public class Cell : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string status) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(status));
+
+        public int X { get; }
+        public int Y { get; }
+
+        private Textures _texture;
+        public Textures Texture {
+            get => _texture;
+            set {
+                _texture = value;
+                OnPropertyChanged(nameof(Texture));
+            }
+        }
+
+        public Cell(int x, int y, Textures texture) {
+            X = x;
+            Y = y;
+            Texture = texture;
+        }
+
+        public static bool operator ==(Cell left, Cell right) => left.X == right.X && left.Y == right.Y;
+
+        public static bool operator !=(Cell left, Cell right) => !(left == right);
+
+
+        public byte[] PointToByteArray() {
+            MemoryStream stream = new MemoryStream();
+            BinaryWriter writer = new BinaryWriter(stream);
+            writer.Write((byte)Texture);
+            writer.Write(X);
+            writer.Write(Y);
+
+            return stream.ToArray();
+        }
+    }
+}
---
+++ WpfSeaBattle/ConnectionWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfSeaBattle {
+    /// <summary>
+    /// Логика взаимодействия для ConnectionWindow.xaml
+    /// </summary>
+    public partial class ConnectionWindow : Window {
+        public string PlayerName { get; set; }
+        public string IpAddress { get; set; }
+        public int Port { get; set; }
+        public ConnectionWindow() {
+            InitializeComponent();
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e) => DialogResult = false;
+
+        private void okButton_Click(object sender, RoutedEventArgs e) {
+            if (string.IsNullOrEmpty(playerNameTextBox.Text)) {
+                MessageBox.Show(
+                    "Введите имя!",
+                    "Ошибка!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                    );
+                return;
+            }
+            if (string.IsNullOrEmpty(ipAddressTextBox.Text)) {
+                MessageBox.Show(
+                    "Введите IP адресс сервера!",
+                    "Ошибка!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                    );
+                return;
+            }
+            if (string.IsNullOrEmpty(ipAddressTextBox.Text)) {
+                MessageBox.Show(
+                    "Введите порт сервера!",
+                    "Ошибка!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                    );
+                return;
+            }
+            PlayerName = playerNameTextBox.Text;
+            IpAddress = ipAddressTextBox.Text;
+            try {
+                Port = int.Parse(portTextBox.Text);
+            }
+            catch (FormatException) {
+
+                MessageBox.Show(
+                    "Номер порта может состоять только из чисел!",
+                    "Ошибка!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                    );
+                return;
+            }
+            DialogResult = true;
+        }
+
+    }
+}
