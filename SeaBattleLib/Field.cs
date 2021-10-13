27b03c51392def778dfdf6bd7bce00d201a18afd
Kutyrev Dmitry <kuty-y4vy@yandex.ru>
2021-10-13 20:31:46 +0300

    V 1.0.0 (#6)
    * create game frame (MVVM)
    
    * add color
    
    * add location and area ship
    
    * create array points and button click
    
    * create lib
    
    * message architecture
    
    * message architecture
    
    * create server
    
    * Revert "create server"
    
    This reverts commit acfdb186c7b650fe176806d389e8e1a50ac237c8.
    
    * create server
    
    * message architecture
    
    * message architecture
    
    * create server
    
    * Revert "create server"
    
    This reverts commit acfdb186c7b650fe176806d389e8e1a50ac237c8.
    
    * create server
    
    * comment
    
    * Генерация рандомных кораблей
    
    * Сервер
    
    * Server
    
    * Server
    
    * Delete ConsoleServer
    
    * Server
    
    * Server
    
    * Server
    
    * Server
    
    * ServerClient
    
    * ServerClient
    
    * Message.GameOver, Message.PlayerHasLeftGame, Chat
    
    * Message.GameOver, Message.PlayerHasLeftGame, Chat
    
    * Server
    
    * Fix Interface
    
    * Chat, LockBattleField
    
    * Chat fix scroll
    
    * New Interface
    
    * Test
    
    * IsAllShipsDestroyed
    
    * IsAllShipsDestroyed
    
    Co-authored-by: 4rt1_j0k3r <artijoker@hotmail.com>
    Co-authored-by: Din67 <2748824@mail.ru>

---
+++ SeaBattleLib/Field.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattleLib {
+    public class Field {
+        private Cell[,] _points;
+
+        public List<Ship> Ships { get; } = new List<Ship>();
+        public int VerticalItemsCount { get; }
+        public int HorizontalItemsCount { get; }
+        public Field(int verticalItemsCount, int horizontalItemsCount, List<Ship> ships = null) {
+            VerticalItemsCount = verticalItemsCount;
+            HorizontalItemsCount = horizontalItemsCount;
+
+            _points = new Cell[verticalItemsCount, horizontalItemsCount];
+            for (int i = 0; i < verticalItemsCount; i++)
+                for (int j = 0; j < horizontalItemsCount; j++)
+                    _points[i, j] = new Cell(i, j, Textures.Water);
+
+            if (ships != null)
+                Ships = ships;
+
+            //Ships.Add(new Ship(new Cell(1, 1, Textures.Ship), 4, Orientation.Vertical));
+
+
+            //ships.ForEach(ship => ship.Area.ForEach(p => points[p.X, p.Y].Texture = p.Texture));
+            Ships.ForEach(ship => ship.Location.ForEach(p => _points[p.X, p.Y].Texture = p.Texture));
+
+        }
+
+        public Cell this[int x, int y] {
+            get => _points[x, y];
+            set => _points[x, y] = value;
+
+        }
+
+        public void AddShip(Ship ship)
+        {
+            Ships.Add(ship);
+            //ships.ForEach(ship => ship.Area.ForEach(p => mass[p.X, p.Y].Value = p.Value));
+            Ships.ForEach(ship => ship.Location.ForEach(p => _points[p.X, p.Y].Texture = p.Texture));
+        }
+
+        /*1 корабль — ряд из 4 клеток(«четырёхпалубный»; линкор)
+        2 корабля — ряд из 3 клеток(«трёхпалубные»; крейсера)
+        3 корабля — ряд из 2 клеток(«двухпалубные»; эсминцы)
+        4 корабля — 1 клетка(«однопалубные»; торпедные катера)*/
+
+        //При размещении корабли не могут касаться друг друга сторонами и углами - Area
+        public static Field GenerateRandomField(int verticalItemsCount, int horizontalItemsCount)
+        {
+            Field field = new Field(verticalItemsCount, horizontalItemsCount);
+            ////Создаем 1 линкор
+            //field.GenerateRandomShips(1, 4);
+            ////Создаем 2 крейсера
+            //field.GenerateRandomShips(2, 3);
+            ////Создаем 3 эсминцы
+            //field.GenerateRandomShips(3, 2);
+            ////Создаем 4 катера
+            //field.GenerateRandomShips(4, 1);
+            for (int i = 1, j = 4; i <= 4 && j >= 1; i++, j--)
+                field.GenerateRandomShips(i, j);
+            return field;
+        }
+
+        private void GenerateRandomShips(int shipsCount, int partCount)
+        {
+            Enumerable.Range(0, shipsCount).ToList().ForEach(_ =>
+            {
+                while (true)
+                {
+                    Ship battleShip = Ship.GenerateRandomShip(VerticalItemsCount, HorizontalItemsCount, partCount);
+                    if (IsLocationIsNotBusy(battleShip))
+                    {
+                        AddShip(battleShip);
+                        break;
+                    }
+                }
+            });
+        }
+
+        private bool IsLocationIsNotBusy(Ship ship)
+        {
+            for (int i = 0; i < ship.Location.Count; i++)
+                if (!(ship.Location[i].X >= 0 && ship.Location[i].X < HorizontalItemsCount)
+                    || !(ship.Location[i].Y >= 0 && ship.Location[i].Y < VerticalItemsCount))
+                    return false;
+
+            foreach (var point in ship.Location)
+                foreach (var ship_ in Ships)
+                    if (ship_.Area.Where(p => p == point).Any() || ship_.Location.Any(p => p == point))
+                        return false;
+            return true;
+        }
+    }
+}
+
---
+++ WpfSeaBattle/IntToColorConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using SeaBattleLib;
+
+namespace WpfSeaBattle {
+    public class IntToColorConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+
+            if (value is null)
+                return null;
+            if (!(value is Textures))
+                throw new ArgumentException($"Исходное значение должно иметь тип {nameof(value)}");
+
+            if (targetType != typeof(Brush))
+                throw new InvalidCastException();
+
+            switch ((Textures)value) {
+                case Textures.Ship: return Brushes.White;
+                case Textures.Water: return Brushes.LightBlue;
+                case Textures.Destroyed: return Brushes.DarkGray;
+                case Textures.Miss: return Brushes.LightCoral;
+                default: throw new ArgumentException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
